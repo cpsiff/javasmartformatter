f97802f2c3c11fd6ae2a54189e09805f64f68d56
Carter Sifferman <cpsiff@gmail.com>
2021-04-13 15:07:33 -0500

    extract variables from command

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "javasmartformatter" is now active!');
+	console.log('Extension "javasmartformatter" is now active');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -16,24 +16,33 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('javasmartformatter.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from JavaSmartFormatter!');
+		let jarPath = "/home/carter/repos/codebuff/target/codebuff-1.5.1.jar";
+		let corpusPath = "/home/carter/Desktop/extension testing/Example.java";
+		let outputPath = "/home/carter/Desktop/extension testing/Output.java";
 
-		// Run a terminal command
-		// vscode.commands.executeCommand(
-		// 	'workbench.action.terminal.sendSequence',
-		// 	{"text": "mkdir test\n"}
-		// );
-		// java -jar /home/carter/repos/codebuff/target/codebuff-1.5.1.jar -g org.antlr.codebuff.Java -rule compilationUnit -corpus "/home/carter/Desktop/extension testing/Example.java" -files java -comment LINE_COMMENT  -o "/home/carter/Desktop/extension testing/Output.java" "/home/carter/Desktop/extension testing/Output.java"
+		let terminalCommand = 'java -jar "' +
+			jarPath +
+			'" -g org.antlr.codebuff.Java -rule compilationUnit -corpus "' +
+			corpusPath +
+			'" -files java -comment LINE_COMMENT -o "' +
+			outputPath +
+			'" "' +
+			outputPath +
+			'"\n';
 
 		vscode.commands.executeCommand(
 			'workbench.action.terminal.sendSequence',
-			{"text": 'java -jar /home/carter/repos/codebuff/target/codebuff-1.5.1.jar -g org.antlr.codebuff.Java -rule compilationUnit -corpus "/home/carter/Desktop/extension testing/Example.java" -files java -comment LINE_COMMENT  -o "/home/carter/Desktop/extension testing/Output.java" "/home/carter/Desktop/extension testing/Output.java"\n'}
+			{"text": terminalCommand}
 		);
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage("reformatted " + outputPath);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log('Extension "javasmartformatter" is now unactive');
+}
